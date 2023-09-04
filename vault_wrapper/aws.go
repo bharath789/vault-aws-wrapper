a715d1bcabde1741334df679df5f9c4a57cc4afb
Bharath Natarajan <bnatarajan@presidio.com>
2023-09-04 16:12:22 +0530

    one more time

--- vault_wrapper/aws.go
+++ vault_wrapper/aws.go
@@ -1,109 +1,80 @@
 package main
 
 import (
-    // "context"
+    "context"
     "fmt"
-    // "os"
+    "os"
     "strings"
 
-    // vault "github.com/hashicorp/vault/api"
-    // auth "github.com/hashicorp/vault/api/auth/aws"
+    vault "github.com/hashicorp/vault/api"
+    auth "github.com/hashicorp/vault/api/auth/aws"
 )
 
-// func getSecretWithAWSAuthIAM() (string, error) {
-//     role := os.Args[1]
-//     config := vault.DefaultConfig() // modify for more granular configuration
+func getSecretWithAWSAuthIAM() (string, error) {
+    role := os.Args[1]
+    config := vault.DefaultConfig() // modify for more granular configuration
 
-//     client, err := vault.NewClient(config)
-//     if err != nil {
-//         return "", fmt.Errorf("unable to initialize Vault client: %w", err)
-//     }
-
-//     awsAuth, err := auth.NewAWSAuth(
-//         auth.WithRole(role), 
-//     )
-//     if err != nil {
-//         return "", fmt.Errorf("unable to initialize AWS auth method: %w", err)
-//     }
-
-//     authInfo, err := client.Auth().Login(context.Background(), awsAuth)
-//     if err != nil {
-//         return "", fmt.Errorf("unable to login to AWS auth method: %w", err)
-//     }
-//     if authInfo == nil {
-//         return "", fmt.Errorf("no auth info was returned after login")
-//     }
-
-    
-    
-//     secretData := os.Args[4]
-//     fmt.Println("print secret data - %v" ,secretData)
-//     // add the secret logic fetch multiple secrets
-//     path, githubOutputVar, keyName := readSecretData(secretData)
-    
-//     // if keyName != "" {
-//     //     fmt.Println("printing from if statement")
-//     //     secret, err := client.KVv2(path).Get(context.Background(), keyName)
-//     // }
-//     // } else {
-//     //     secret := client.KVv2(path).Get()
-//     // } 
-    
-    
-//     // if err != nil {
-//     //     return "", fmt.Errorf("unable to read secret: %w", err)
-//     // }
-
-//     // data map can contain more than one key-value pair,
-//     // in this case we're just grabbing one of them
-    
-//     secret, err := client.KVv2("dev/kvv2").Get(context.Background(), "example")
-//     fmt.Printf("printing the secret: %v\n" , secret)
-//     fmt.Printf("printng the path: %v\n", path)
-//     fmt.Printf("printing githubOutputVar: %v\n", githubOutputVar)
-//     fmt.Printf("printing keyName: %v\n", keyName)
-//     // value, ok := secret.Data["password"].(string)
-//     // if !ok {
-//     //     return "", fmt.Errorf("value type assertion failed: %T %#v", secret.Data["password"], secret.Data["password"])
-//     // }
-
-//     return "secret", nil
-// }
-
-// func main() {
-//     if len(os.Args) < 4 {
-//         fmt.Println("Usage: ./aws.go $ROLE_NAME $VAULT_ADDR $VAULT_NAMESPACE")
-//         return
-//     }
-//     // setting env namespace, url
-//     os.Setenv("VAULT_ADDR", os.Args[3])
-//     os.Setenv("VAULT_NAMESPACE", os.Args[2])
-
-//     secretValue, err := getSecretWithAWSAuthIAM()
-//     if err != nil {
-//         fmt.Printf("Error: %v\n", err)
-//         return
-//     }
-
-//     // fmt.Println("Secret Value:", secretValue)
-//     os.Setenv("GITHUB_OUTPUT", secretValue)
-// }
-
-// func readSecretData(data string) (string, string, string){
-
-    func formatSeretPath(fmtPath string) (string){
-        var fmtPathData string
-        secretsPathNew := strings.Split(fmtPath, "/")
-        
-        for i :=0; i < (len(secretsPathNew) - 1); i++ {
-            fmtPathData += secretsPathNew[i] + "/"
-        }
-		return fmtPathData
+    client, err := vault.NewClient(config)
+    if err != nil {
+        return "", fmt.Errorf("unable to initialize Vault client: %w", err)
     }
 
-    func main() {
+    awsAuth, err := auth.NewAWSAuth(
+        auth.WithRole(role), 
+    )
+    if err != nil {
+        return "", fmt.Errorf("unable to initialize AWS auth method: %w", err)
+    }
 
-	data := "dev/kvv2/example foo | MY_PASSWORD"
+    authInfo, err := client.Auth().Login(context.Background(), awsAuth)
+    if err != nil {
+        return "", fmt.Errorf("unable to login to AWS auth method: %w", err)
+    }
+    if authInfo == nil {
+        return "", fmt.Errorf("no auth info was returned after login")
+    }
+
+    
+    
+    secretData := os.Args[4]
+    fmt.Println("print secret data - %v" ,secretData)
+    // add the secret logic fetch multiple secrets
+    path, githubOutputVar, keyName := readSecretData(secretData)
+    
+    secret, err := client.KVv2("dev/kvv2").Get(context.Background(), "example")
+    fmt.Printf("printing the secret: %v\n" , secret)
+    fmt.Printf("printng the path: %v\n", path)
+    fmt.Printf("printing githubOutputVar: %v\n", githubOutputVar)
+    fmt.Printf("printing keyName: %v\n", keyName)
+    value, ok := secret.Data[keyName].(string)
+    if !ok {
+        return "", fmt.Errorf("value type assertion failed: %T %#v", secret.Data[keyName], secret.Data[keyName])
+    }
+
+    return value, nil
+}
+
+func main() {
+    if len(os.Args) < 4 {
+        fmt.Println("Usage: ./aws.go $ROLE_NAME $VAULT_ADDR $VAULT_NAMESPACE")
+        return
+    }
+    // setting env namespace, url
+    os.Setenv("VAULT_ADDR", os.Args[3])
+    os.Setenv("VAULT_NAMESPACE", os.Args[2])
+
+    secretValue, err := getSecretWithAWSAuthIAM()
+    if err != nil {
+        fmt.Printf("Error: %v\n", err)
+        return
+    }
+
+    fmt.Println("Secret Value:", secretValue)
+    os.Setenv("GITHUB_OUTPUT", secretValue)
+}
+
+func readSecretData(data string) (string, string, string){
+	// data := "dev/kvv2/example foo | MY_PASSWORD"
 
     var (
         secretsPath string
@@ -115,7 +86,8 @@ import (
 	parts := strings.Split(data, " ")
 	if len(parts) >= 4 {
         secretsPath = parts[0]
-        secretsPathSplit = formatSeretPath(secretsPath)	
+        secretsPathSplit = formatSeretPath(secretsPath)
+        fmt.Println("Secrets Directory:", secretsPathSplit)	
 	} else {
         secretsPath = parts[0]
         secretsPathSplit = formatSeretPath(secretsPath)
@@ -137,5 +109,15 @@ import (
 		fmt.Println("key:", key)
 	}
 
-    // return secretsPathSplit, key, variable 
+    return secretsPathSplit, key, variable 
+}
+
+func formatSeretPath(fmtPath string) (string){
+    var fmtPathData string
+    secretsPathNew := strings.Split(fmtPath, "/")
+    
+    for i :=0; i < (len(secretsPathNew) - 1); i++ {
+        fmtPathData += secretsPathNew[i] + "/"
+    }
+    return fmtPathData
 }
